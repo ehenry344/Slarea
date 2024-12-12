993910fb9ef29c0fd09cc370425e0f160588fe88
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2024-12-12 16:19:18 -0500

    Update Types.lua

--- src/Client/InterfaceController/Types.lua
+++ src/Client/InterfaceController/Types.lua
@@ -5,80 +5,37 @@
 export type SlareaFrameProto = {
 	__index: SlareaFrameProto,
 
-	new: (frameId: number) -> SlareaFrame,
-	Mount: (SlareaFrame, ScreenGui) -> (),
-
+	new: (name: string, screenGui: ScreenGui) -> SlareaFrame,
+	AddPlot: (SlareaFrame, LinePlot) -> (),
+	
 	_accommodate: (SlareaFrame) -> ()
 }
 
 export type SlareaFrame = typeof(setmetatable({} :: {
-	_frame: Frame,
+	Frame: Frame,
 	
-	_plots: {Plot}
+	_plots: {LinePlot}
 }, {} :: SlareaFrameProto))
-
--- Plot Element Types 
-
--- T = base element type
-
-type BaseElementProto<P, T> = {
-	__index: P,
-	
-	new: () -> T,
-	Mount: (T, Frame) -> ()
-}
-
--- Bar Element (For Bar Graphs)
-
-export type BarElementProto = BaseElementProto<BarElementProto, BarElement> & {
-	SetPosition: (BarElement, pos: Vector2, xOffset: number, yOffset: number) -> (),
-	SetSize: (BarElement, Vector2) -> ()
-}
-
-export type BarElement = typeof(setmetatable({} :: {
-	_guiObject: Frame
-}, {} :: BarElementProto))
-
--- Point Element
-
-export type PointElementProto = BaseElementProto<PointElementProto, PointElement> & {
-	SetPosition: (PointElement, pos: Vector2, xOffset: number, yOffset: number) -> ()
-}
-
-export type PointElement = typeof(setmetatable({} :: {
-	_guiObject: Frame
-}, {} :: PointElementProto))
 
 -- Plot Types
 
 -- Base Plot Type 
-type BasePlotProto<P, T> = {
-	__index: P,
+
+-- Line Plot
+
+export type LinePlotProto = {
+	__index: LinePlotProto,
 	
-	new: (Frame) -> T,
+	new: ()->LinePlot,
+	PlotData: (LinePlot, {Vector2})->(),
+	ClearPlot: (LinePlot) -> (),
 }
 
--- Bar Plot
+export type LinePlot = typeof(setmetatable({} :: {
+	Frame: Frame,
+	_points: {Frame},
+	_lines: {Frame}
+}, {} :: LinePlotProto))
 
-export type BarPlotProto = BasePlotProto<BarPlotProto, BarPlot> & {
-		
-} -- nothing for now
-
-export type BarPlot = typeof(setmetatable({} :: {
-	_frame: Frame,
-	_bars: {BarElement}
-}, {} :: BarPlotProto))
-
-
--- Scatter Plot
-
-export type ScatterPlotProto = BasePlotProto<ScatterPlotProto, ScatterPlot> & {}
-
-export type ScatterPlot = typeof(setmetatable({} :: {
-	_frame: Frame,
-	_points: {PointElement}
-}, {} :: ScatterPlotProto))
-
-export type Plot = BarPlot | ScatterPlot
 
 return nil
