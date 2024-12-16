a9f4ccf5b99116f2865b6b5fbcaaca859e98f312
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2024-12-16 13:32:19 -0500

    Update Config.lua

--- src/Client/InterfaceController/Config.lua
+++ src/Client/InterfaceController/Config.lua
@@ -4,37 +4,64 @@ return table.freeze({
 			SlareaFrame = {
 				Frame = {
 					AnchorPoint = Vector2.new(0.5, 0.5),
-					Position = UDim2.fromScale(0.5, 0.5)
+					Position = UDim2.fromScale(0.5, 0.5),
+					BorderSizePixel = 0,
+					BackgroundTransparency = 1,
+				},
+				Plots = {
+					Margin = Vector2.new(50, 50)
 				}
 			},
 			
 			Plots = {
-				Scatter = {
+				Line = {
 					Frame = {
-						Size = UDim2.fromOffset(500, 300)
+						Size = UDim2.fromOffset(500, 300),
+						BackgroundColor3 = Color3.fromRGB(34, 34, 34),
+						BorderSizePixel = 1,
+						BorderColor3 = Color3.fromRGB(230, 230, 230)
 					},
-				},
-				Bar = {
-					Frame = {
-						Size = UDim2.fromOffset(500, 300)
+					Bounds = {
+						Left = 0,
+						Right = 0,
+						Bottom = 0.1,
+						Top = 0.1 
+					},
+					Padding = {
+						Left = 0.1,
+						Right = 0.1,
+						Bottom = 0,
+						Top = 0, 
 					}
+				},
+			},
+			
+			PlotElements = {
+				Axis = {
+					Size = {
+						Width = 50,
+						Height = 50
+					},
+					Frame = {
+						BackgroundTransparency = 1,
+					}, 
 				}
 			},
 			
 			PlotComponents = {
 				Point = {
 					Frame = {
-						Size = UDim2.fromOffset(2, 2),
+						Size = UDim2.fromOffset(5, 5),
 						BackgroundColor3 = Color3.fromRGB(0, 170, 255),
 						BorderSizePixel = 0 
 					}
 				},
-				Bar = {
+				Line = {
 					Frame = {
-						BackgroundColor3 = Color3.fromRGB(0, 170, 255), 
+						BackgroundColor3 = Color3.fromRGB(0, 170, 255),
 						BorderSizePixel = 0
-					}
-				}
+					},
+				},
 			}
 		}
 	}
